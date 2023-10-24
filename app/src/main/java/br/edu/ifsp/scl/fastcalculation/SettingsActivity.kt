9707272edf490b9052c9e83a81059a35c11cccd3
Gabrielle <gabrielleangelipoli@gmail.com>
2023-10-24 19:00:02 -0300

    Renomeando activity

--- app/src/main/java/br/edu/ifsp/scl/fastcalculation/SettingsActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/fastcalculation/SettingsActivity.kt
@@ -3,9 +3,9 @@ package br.edu.ifsp.scl.fastcalculation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_settings)
     }
 }
