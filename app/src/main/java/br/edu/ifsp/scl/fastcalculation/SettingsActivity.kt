035d820c3370567061350aa0f4fe07d545245278
Gabrielle <gabrielleangelipoli@gmail.com>
2023-10-24 19:39:48 -0300

    Criando layout

--- app/src/main/java/br/edu/ifsp/scl/fastcalculation/SettingsActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/fastcalculation/SettingsActivity.kt
@@ -2,10 +2,16 @@ package br.edu.ifsp.scl.fastcalculation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.edu.ifsp.scl.fastcalculation.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
+
+    private val activitySettingsBinding: ActivitySettingsBinding by lazy {
+        ActivitySettingsBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        setContentView(activitySettingsBinding.root)
     }
 }
