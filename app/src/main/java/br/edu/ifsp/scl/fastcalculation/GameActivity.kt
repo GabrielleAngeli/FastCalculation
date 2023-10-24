4eeb06211d968c65e94795f4e3f365b71be179ee
Gabrielle <gabrielleangelipoli@gmail.com>
2023-10-24 20:27:50 -0300

    Recebendo valores da settingsActivity

--- app/src/main/java/br/edu/ifsp/scl/fastcalculation/GameActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/fastcalculation/GameActivity.kt
@@ -2,10 +2,26 @@ package br.edu.ifsp.scl.fastcalculation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.edu.ifsp.scl.fastcalculation.Extras.EXTRA_SETTINGS
+import br.edu.ifsp.scl.fastcalculation.databinding.ActivityGameBinding
 
 class GameActivity : AppCompatActivity() {
+
+    private val activityGameBinding: ActivityGameBinding by lazy {
+        ActivityGameBinding.inflate(layoutInflater)
+    }
+
+    private lateinit var settings: Settings
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game)
+        setContentView(activityGameBinding.root)
+
+        setSupportActionBar(activityGameBinding.gameTbIn.gameTb)
+        supportActionBar?.apply {
+            title = getString(R.string.app_name)
+            subtitle = getString(R.string.game)
+        }
+
+        settings = intent.getParcelableExtra<Settings>(EXTRA_SETTINGS) ?: Settings() //obsoleto para a versão em que estamos utilizando
     }
 }
